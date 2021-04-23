d8567368ac168ed93843ae407c01192d4db8b438
David Minge <minge.david@gmail.com>
2021-04-23 08:53:57 -0800

    Added Routes to create, edit, home pages

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,25 @@
-import './App.css';
+import React, { Component } from "react";
+import { BrowserRouter as Router, Route, Link } from "react-router-dom"
+import { Home } from "./components/home.component"
+import { Edit } from "./components/edit.component"
+import { Create } from "./components/create.component"
 
 function App() {
   return (
-    <div>
-
-    </div>
+    <Router>
+      <div>
+        <h2>GTD APP</h2>
+        <Route path="/" exact>
+          <Home />
+        </Route>
+        <Route path="/edit/:id">
+          <Edit />
+        </Route>
+        <Route path="/create">
+          <Create />
+        </Route>
+      </div>
+    </Router>
   );
 }
 
